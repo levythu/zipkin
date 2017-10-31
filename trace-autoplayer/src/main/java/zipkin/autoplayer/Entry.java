e51e658e60551ac70627c6f401e587b8597bbaa8
Levy <zly.george@163.com>
2017-10-31 15:35:14 -0400

    ch

--- trace-autoplayer/src/main/java/zipkin/autoplayer/Entry.java
+++ trace-autoplayer/src/main/java/zipkin/autoplayer/Entry.java
@@ -7,7 +7,10 @@ import java.lang.Throwable;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.Date;
+import java.util.List;
 import javax.annotation.Nullable;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Arrays;
 
 import zipkin.Span;
 import zipkin.Codec;
@@ -38,10 +41,10 @@ public class Entry {
     //   .build();
 
     // DeltaFS:
-    // return DeltaFSStorage.builder()
-    //   .strictTraceId(true)
-    //   .maxSpanCount(Integer.MAX_VALUE)
-    //   .build();
+    return DeltaFSStorage.builder()
+      .strictTraceId(true)
+      .maxSpanCount(Integer.MAX_VALUE)
+      .build();
 
     // MySQL
     // HikariDataSource result = new HikariDataSource();
@@ -57,17 +60,17 @@ public class Entry {
     //     .build();
 
     // Cassandra3
-    return Cassandra3Storage.builder()
-        .keyspace("zipkin3")
-        .contactPoints("localhost")
-        // .localDc("ec2-54-236-232-202.compute-1.amazonaws.com")
-        .maxConnections(100)
-        .ensureSchema(true)
-        .useSsl(false)
-        .username("cassandra")
-        .password("cassandra")
-        .indexFetchMultiplier(3)
-        .build();
+    // return Cassandra3Storage.builder()
+    //     .keyspace("zipkin3")
+    //     .contactPoints("localhost")
+    //     // .localDc("ec2-54-236-232-202.compute-1.amazonaws.com")
+    //     .maxConnections(100)
+    //     .ensureSchema(true)
+    //     .useSsl(false)
+    //     .username("cassandra")
+    //     .password("cassandra")
+    //     .indexFetchMultiplier(3)
+    //     .build();
   }
 
   public static int parseCmdLine(String[] args, int pos, int def) {
@@ -99,19 +102,24 @@ public class Entry {
 
   public long throttleReq = 50;
 
+  public Span applyDelta(Span original, int delta, int totTime) {
+    if (delta > 0) {
+      Span.Builder newSpanBuilder = (new Span.Builder(original));
+      newSpanBuilder.id(original.id * totTime + delta);
+      if (original.parentId != null)
+        newSpanBuilder.parentId(original.parentId * totTime + delta);
+      newSpanBuilder.traceId(original.traceId * totTime + delta);
+      newSpanBuilder.traceIdHigh( original.traceIdHigh * totTime + delta);
+      original = newSpanBuilder.build();
+    }
+    return original;
+  }
+
   public void putInStorage(String[] str, int len, AsyncSpanConsumer storage, int timeDelta, int totTime) {
     ArrayList<Span> spans = new ArrayList<Span>();
     for (int i = 0; i < len; i++) {
       Span sp = Codec.JSON.readSpan(str[i].getBytes());
-      if (timeDelta > 0) {
-        sp = (new Span.Builder(sp))
-            .id(sp.id * totTime + timeDelta)
-            .parentId(sp.parentId * totTime + timeDelta)
-            .traceId(sp.traceId * totTime + timeDelta)
-            .traceIdHigh( sp.traceIdHigh * totTime + timeDelta)
-            .build();
-      }
-      spans.add(sp);
+      spans.add(applyDelta(sp, timeDelta, totTime));
     }
     long currentTask;
     synchronized (task) {
@@ -178,23 +186,73 @@ public class Entry {
     }
   }
 
+  public void RunRead(String file, int times, int totTime) throws Throwable {
+    System.out.println(
+        "Read benchmark from " + file + " " + Integer.toString(times) +
+        "times, with scala = " + Integer.toString(totTime));
+
+    long avgResponseTime = 0;
+    long totalRec = 0;
+
+    StorageComponent storage = getTestedStorageComponent();
+    SpanStore sstore = storage.spanStore();
+
+    source = new BufferedReader(new FileReader(file));
+    String[] res = new String[222222];
+    int actualLen = readLines(222222, res);
+    ArrayList<Span> spans = new ArrayList<Span>();
+    for (int i = 0; i < actualLen; i++) {
+      Span sp = Codec.JSON.readSpan(res[i].getBytes());
+      spans.add(sp);
+    }
+
+    for (int i = 0; i < times; i++) {
+      int spanBucket = ThreadLocalRandom.current().nextInt(0, actualLen);
+      int timeDelta = ThreadLocalRandom.current().nextInt(0, totTime);
+      Span thisSpan = applyDelta(spans.get(spanBucket), timeDelta, totTime);
+      long startTS = (new Date()).getTime();
+      List<Span> resTrace = sstore.getTrace(thisSpan.id);
+      long endTS = (new Date()).getTime();
+
+      if (resTrace.size() == 0) {
+        System.out.println("WARN: traceID = " + Long.toString(thisSpan.id) + " not found");
+        continue;
+      }
+
+      avgResponseTime += endTS - startTS;
+      totalRec++;
+      {
+        System.out.println(Double.toString(((double)avgResponseTime) / totalRec));
+      }
+    }
+
+    System.out.println("AVG latency: " + Double.toString(((double)avgResponseTime) / totalRec));
+  }
+
   public static void main(String[] args) throws Throwable {
       System.out.println("Hello, World");
       String file = args[0];
       int times = parseCmdLine(args, 1, 1);
       int blockSize = parseCmdLine(args, 2, 100);
-
       Entry me = new Entry();
-      long startTS = (new Date()).getTime();
-      me.Run(file, times, blockSize);
-      System.out.println("Sleep");
-      while (true) {
-        TimeUnit.SECONDS.sleep(1);
-        synchronized (me.task) {
-          if (me.task == 0) {
-            System.out.println("Done, total time = " + Long.toString(me.lastTS - startTS));
-            System.out.println("Total records = " + Long.toString(me.totalRecord));
-            System.out.println("Throughput = " + Double.toString((  (double)me.totalRecord) * 1000 / (double)(me.lastTS - startTS)  ));
+
+      System.out.println(Arrays.toString(args));
+      if (args.length > 3 && args[3].equals("READ")) {
+        // xxx filename, readtimes, scalaFactor READ
+        me.RunRead(file, times, blockSize);
+      } else {
+        // xxx filename, writeRuns, batchSize
+        long startTS = (new Date()).getTime();
+        me.Run(file, times, blockSize);
+        System.out.println("Sleep");
+        while (true) {
+          TimeUnit.SECONDS.sleep(1);
+          synchronized (me.task) {
+            if (me.task == 0) {
+              System.out.println("Done, total time = " + Long.toString(me.lastTS - startTS));
+              System.out.println("Total records = " + Long.toString(me.totalRecord));
+              System.out.println("Throughput = " + Double.toString((  (double)me.totalRecord) * 1000 / (double)(me.lastTS - startTS)  ));
+            }
           }
         }
       }
